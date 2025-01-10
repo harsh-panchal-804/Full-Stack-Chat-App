830d4bedfa3797177aea52a619bd8d81a073822e
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-01-10 15:25:55 +0530

    Remove Esm

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,32 +1,40 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const {app,server,io}=require('./lib/socket.js');
-const path =require("path");
+const { app, server, io } = require('./lib/socket.js');
+const path = require('path');
 dotenv.config();
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: 'http://localhost:5173',
-    credentials: true
-}));
+    credentials: true,
+  })
+);
+
 const authRoutes = require('./routes/auth.route.js');
 const messageRoutes = require('./routes/message.routes.js');
 const { connectDB } = require('./lib/db.js');
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../FrontEnd/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../FrontEnd/dist/index.html"));
-    })
-}
-const port=process.env.PORT || 5001;
-server.listen(port, () => {
-    console.log('Server is running on '+ port);
-    connectDB();
-});
 
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../FrontEnd/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../FrontEnd/dist/index.html'));
+  });
+}
+
+const port = process.env.PORT || 5001;
+server.listen(port, () => {
+  console.log('Server is running on ' + port);
+  connectDB();
+});
